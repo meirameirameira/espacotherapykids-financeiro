eea6cffc78d37a2119b829080ba264eb3fc7235f
Felipe Meira <meirafelipe.macedo@gmail.com>
2025-05-04 07:43:25 -0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -7,45 +7,70 @@ public class Main {
         System.out.print("\nDigite o nome do paciente: ");
         String nome = sc.nextLine();
 
-        System.out.println("\nDados de Fono");
-        System.out.print(" Valor da sessão: R$");
-        double fonoPreco = sc.nextDouble();
-        System.out.print(" Horas de sessão: ");
-        int fonoHoras = sc.nextInt();
-        System.out.print(" Valor do reembolso: R$");
-        double fonoReembolso = sc.nextDouble();
+        int op;
+        Sessao fono = null;
+        Sessao to = null;
 
-        Sessao fono = new Sessao(fonoPreco, fonoHoras, fonoReembolso);
-        System.out.println("\nInformações da Fono");
-        System.out.println(" Valor da sessão: R$" + fono.getPreco());
-        System.out.println(" Horas de sessão: " + fono.getHoras());
-        System.out.println(" Valor Total: R$" + fono.calcularTotal());
-        System.out.println(" Valor do reembolso: R$" + fono.calcularReembolso());
-        System.out.println(" Sessões para Nota Fiscal: " + fono.calcularNF());
+        do {
+            System.out.println("\nEscolha uma opção: \n1-Fono \n2-TO \n3-ABA \n4-Exibir Informações \n0-Sair");
+            op = sc.nextInt();
 
-        System.out.println("----------------------------------");
+            switch (op) {
+                case 1:
+                    System.out.println("\nDados de Fono:");
+                    System.out.print(" Valor da sessão: R$");
+                    double fonoPreco = sc.nextDouble();
+                    System.out.print(" Horas de sessão: ");
+                    int fonoHoras = sc.nextInt();
+                    System.out.print(" Valor do reembolso: R$");
+                    double fonoReembolso = sc.nextDouble();
 
-        System.out.println("\nDados de TO");
-        System.out.print(" Valor da sessão: R$");
-        double toPreco = sc.nextDouble();
-        System.out.print(" Horas de sessão: ");
-        int toHoras = sc.nextInt();
-        System.out.print(" Valor do reembolso: R$");
-        double toReembolso = sc.nextDouble();
+                    fono = new Sessao(fonoPreco, fonoHoras, fonoReembolso);
+                    System.out.println("\nInformações da Fono:");
+                    System.out.println(" Valor da sessão: R$" + fono.getPreco());
+                    System.out.println(" Horas de sessão: " + fono.getHoras());
+                    System.out.println(" Valor Total: R$" + fono.calcularTotal());
+                    System.out.println(" Valor do reembolso: R$" + fono.calcularReembolso());
+                    System.out.println(" Sessões para Nota Fiscal: " + fono.calcularNF());
 
-        Sessao to = new Sessao(toPreco, toHoras, toReembolso);
-        System.out.println("\nInformações da TO");
-        System.out.println(" Valor da sessão: R$" + to.getPreco());
-        System.out.println(" Horas de sessão: " + to.getHoras());
-        System.out.println(" Valor Total: R$" + to.calcularTotal());
-        System.out.println(" Valor reembolso: R$" + to.calcularReembolso());
-        System.out.println(" Sessões para Nota Fiscal: " + to.calcularNF());
+                    break;
+                case 2:
+                    System.out.println("\nDados de TO:");
+                    System.out.print(" Valor da sessão: R$");
+                    double toPreco = sc.nextDouble();
+                    System.out.print(" Horas de sessão: ");
+                    int toHoras = sc.nextInt();
+                    System.out.print(" Valor do reembolso: R$");
+                    double toReembolso = sc.nextDouble();
 
-        System.out.println("----------------------------------");
+                    to = new Sessao(toPreco, toHoras, toReembolso);
+                    System.out.println("\nInformações da TO:");
+                    System.out.println(" Valor da sessão: R$" + to.getPreco());
+                    System.out.println(" Horas de sessão: " + to.getHoras());
+                    System.out.println(" Valor Total: R$" + to.calcularTotal());
+                    System.out.println(" Valor reembolso: R$" + to.calcularReembolso());
+                    System.out.println(" Sessões para Nota Fiscal: " + to.calcularNF());
 
-        Paciente paciente = new Paciente(nome, fono, to);
-        paciente.exibirInformacoes();
+                    break;
+                case 3:
+                    System.out.println("Indisponível no momento.");
 
-        //Paciente paciente = new Paciente(nome, fono, to);
+                    break;
+                case 4:
+                    if (fono != null && to != null) {
+                        Paciente paciente = new Paciente(nome, fono, to);
+                        paciente.exibirInformacoes();
+                    } else {
+                        System.out.println("Você precisa preencher as sessões de Fono e TO antes.");
+                    }
+
+                    break;
+                case 0:
+                    System.out.println("Finalizando programa.");
+
+                    break;
+            }
+           }while (op != 0);
+                sc.close();
     }
 }
